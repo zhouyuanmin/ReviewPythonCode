95cb568a8c01aa0293dfe3d3e3ff327de6d25535
Yuanmin Zhou <1837722596@qq.com>
2020-11-09 13:48:46 +0800

    fastapi对外的api接口

---
+++ src/reviews/review_word_cloud.py
@@ -0,0 +1,100 @@
+import os
+import json
+from typing import List, Dict
+from tempfile import mkstemp
+import asyncio
+import aiofiles
+import uvicorn
+from fastapi import FastAPI
+from pydantic import BaseModel
+from src.mock.mock import mock_word_cloud
+
+app = FastAPI()
+
+
+# 声明参数模型
+class Item(BaseModel):
+    code: str
+    answer: Dict
+    scores: Dict
+
+
+# 替换词云源代码
+mock_word_cloud("/Users/zhou/Downloads/ddc_oj")
+
+
+async def review_word_cloud(code: str, answer: Dict, scores: Dict, debug=False):
+    # 将code写入文件
+    tmp_fp, tmp_file_name = mkstemp(suffix=".py", dir='.', text=True)
+    async with aiofiles.open(tmp_file_name, "w") as f:
+        await f.write(code)
+
+    # 创建子进程执行代码
+    process = await asyncio.create_subprocess_exec(
+        *['python', '-sB', tmp_file_name],
+        stdout=asyncio.subprocess.PIPE,
+        stdin=asyncio.subprocess.PIPE,
+        stderr=asyncio.subprocess.PIPE,
+        cwd='.',
+        close_fds=True
+    )
+
+    # 评测词云，打分
+    score = 0
+    while process.returncode is None:
+        try:
+            line_data: bytes = await asyncio.wait_for(process.stdout.readline(), 2)
+            if b'e485c73f5dd07c29' in line_data:
+                review_data: str = line_data.decode('u8')
+                review_data: Dict = json.loads(review_data)
+                if debug:
+                    print(f'debug:review_data-->{review_data}')
+                for key, value in answer.items():
+                    # 字符串或数值，则比较值是否相等
+                    if isinstance(value, (str, int)):
+                        tmp_score = scores.get(key) if value == review_data.get(key, None) else 0
+                    elif isinstance(value, List):
+                        tmp_score = scores.get(key) if set(value) == set(review_data.get(key, None)) else 0
+                    elif isinstance(value, Dict):
+                        tmp_score = scores.get(key) if value == review_data.get(key, None) else 0
+                    else:
+                        tmp_score = 0
+                    score += tmp_score
+            else:
+                if debug:
+                    print(f'debug:line_data-->{line_data.decode("u8")}')  # 打印其他数据
+        except asyncio.TimeoutError:
+            lines_data: bytes = await asyncio.wait_for(process.stdout.read(1024), 2)
+            if debug:
+                print(f'debug:lines_data-->{lines_data.decode("u8")}')  # 数据不是多行，不走这里
+
+    # 还原环境，删除临时文件
+    os.remove(tmp_file_name)
+
+    # 返回结果
+    result = {
+        'status': 200,
+        'score': score,
+    }
+
+    # 程序异常情况
+    out, err = await process.communicate()
+    if err:
+        result['status'] = 400
+        result['error'] = err.decode('u8')
+
+    if debug:
+        print(f'debug:result-->{result}')
+    return result
+
+
+@app.post("/reviews/wordcloud")
+async def read_root(item: Item):
+    # 是否debug
+    debug = False
+    result = await review_word_cloud(code=item.code, answer=item.answer, scores=item.scores, debug=debug)
+    return result
+
+
+if __name__ == '__main__':
+    uvicorn.run(app='review_word_cloud:app', host="127.0.0.1", port=8000, reload=True, debug=True)
